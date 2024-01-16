3f6c31cad405eb2e7a73978534fc545c1ded90e3
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2024-01-16 20:26:14 +0200

    js

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginAndSign from "./Components/LoginAndSign.jsx"
+const App = () => {
+  return (
+    <LoginAndSign/>
+  )
+}
+export default App
---
+++ client/src/Components/LoginAndSign.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const LoginAndSign = () => {
+    return (
+    <div>
+      <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+            />
+            <button>Register</button>
+          </div>
+       
+          <button>Register</button>
+        
+        <button>Login</button>
+      </div>
+    </div>
+)
+}
+export default LoginAndSign
